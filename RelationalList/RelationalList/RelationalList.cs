9307dcfc4b79c3249a894b9d818e3c6893b52e47
clee-dev <codyleedev@gmail.com>
2018-12-03 22:25:01 -0600

    add

---
+++ RelationalList/RelationalList/RelationalList.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RelationalList
+{
+    public static class LINQExtension
+    {
+        public static RelationalList<T1, T2> ToRelationalList<T1, T2>(this IEnumerable<T1> x, Func<T1, T2> selector)
+        {
+            RelationalList<T1, T2> ret = new RelationalList<T1, T2>();
+            ret.X = x.ToList();
+            ret.Y = (from s in x select selector(s)).ToList();
+            return ret;
+        }
+
+        public static RelationalList<T1, T2> ToRelationalList<T1, T2>(this IEnumerable<T1> x, IEnumerable<T2> y)
+        {
+            RelationalList<T1, T2> ret = new RelationalList<T1, T2>();
+            ret.X = x.ToList();
+            ret.Y = y.ToList();
+            return ret;
+        }
+    }
+
+    public class RelationalListPair<T1, T2>
+    {
+        public T1 X { get; set; }
+        public T2 Y { get; set; }
+
+        public RelationalListPair(T1 x, T2 y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class RelationalList<T1, T2> : IEnumerable
+    {
+        public List<T1> X = new List<T1>();
+        public List<T2> Y = new List<T2>();
+
+        public int Count => X.Count;
+        public int Capacity => X.Capacity + Y.Capacity;
+
+        public T2 this[T1 i]
+        {
+            //RelationList<int, string> X = new RelationalList<int, string>();
+            //int n = 5;
+            //string s = "test";
+            //X[n] = s;
+
+            get
+            {
+                int x = X.IndexOf(i);
+                return Y[x];
+            }
+            set
+            {
+                int x = X.IndexOf(i);
+                Y[x] = (T2)value;
+            }
+        }
+
+        public T1 this[T2 i]
+        {
+            //RelationList<int, string> X = new RelationalList<int, string>();
+            //int n = 13;
+            //string s = "yes";
+            //X[s] = n;
+
+            get
+            {
+                int x = Y.IndexOf(i);
+                return X[x];
+            }
+            set
+            {
+                int x = Y.IndexOf(i);
+                X[x] = (T1)value;
+            }
+        }
+
+        public RelationalListPair<T1, T2> ElementAt(int index) => new RelationalListPair<T1, T2>(X[index], Y[index]);
+
+        public void Add(T1 x, T2 y)
+        {
+            for (int i = 0; i < X.Count; i++)
+                if (X[i].Equals(x) && Y[i].Equals(y)) throw new Exception("Can't add duplicate RelationalListPair.");
+
+            X.Add(x);
+            Y.Add(y);
+        }
+
+        public void Add(RelationalListPair<T1, T2> item)
+        {
+            Add(item.X, item.Y);
+        }
+
+        public void Remove(T1 x, T2 y)
+        {
+            for (int i = 0; i < X.Count; i++)
+            {
+                if (X[i].Equals(x) && Y[i].Equals(y))
+                {
+                    X.RemoveAt(i);
+                    Y.RemoveAt(i);
+                    return;
+                }
+            }
+            throw new Exception("RelationalListPair not found.");
+        }
+
+        public void Remove(RelationalListPair<T1, T2> item)
+        {
+            Remove(item.X, item.Y);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= X.Count || index < 0) throw new IndexOutOfRangeException("Index cannot be less than 0 or greater than RelationalList.Count - 1.");
+            X.RemoveAt(index);
+            Y.RemoveAt(index);
+        }
+
+        public void Clear()
+        {
+            X.Clear();
+            Y.Clear();
+        }
+
+        public int IndexOfX(T1 item) => X.IndexOf(item);
+
+        public int IndexOfY(T2 item) => Y.IndexOf(item);
+
+        public int IndexOf(T1 x, T2 y)
+        {
+            for (int i = 0; i < X.Count; i++)
+                if (X[i].Equals(x) && Y[i].Equals(y)) return i;
+            return -1;
+        }
+
+        public int IndexOf(RelationalListPair<T1, T2> item) => IndexOf(item.X, item.Y);
+
+        public void Insert(T1 x, T2 y, int index)
+        {
+            for (int i = 0; i < X.Count; i++)
+                if (X[i].Equals(x) && Y[i].Equals(y)) throw new Exception("Can't add duplicate RelationalListPair.");
+
+            X.Insert(index, x);
+            Y.Insert(index, y);
+        }
+
+        public void Insert(RelationalListPair<T1, T2> item, int index)
+        {
+            Insert(item.X, item.Y, index);
+        }
+
+        public bool Contains(T1 x, T2 y)
+        {
+            if (!X.Contains(x) || !Y.Contains(y)) return false;
+            for (int i = 0; i < X.Count; i++)
+                if (X[i].Equals(x) && Y[i].Equals(y)) return true;
+            return false;
+        }
+
+        public bool Contains(RelationalListPair<T1, T2> item) => Contains(item.X, item.Y);
+
+        public bool ContainsX(T1 item) => X.Contains(item);
+
+        public bool ContainsY(T2 item) => Y.Contains(item);
+
+        /// <summary>
+        /// Not immune to duplicates. Exception thrown afterward if duplicates were added.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void AddRange(IEnumerable<T1> x, IEnumerable<T2> y, bool checkDuplicates = true)
+        {
+            foreach (var item in x)
+                X.Add(item);
+            foreach (var item in y)
+                Y.Add(item);
+            if (checkDuplicates && DuplicatesFound()) throw new Exception("Duplicate RelationalListPairs found after AddRange() completed.");
+        }
+
+        /// <summary>
+        /// Not immune to duplicates. Exception thrown afterward if duplicates were added.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void InsertRange(int index, IEnumerable<T1> x, IEnumerable<T2> y, bool checkDuplicates = true)
+        {
+            foreach (var item in x)
+                X.Insert(index, item);
+            foreach (var item in y)
+                Y.Insert(index, item);
+            if (DuplicatesFound()) throw new Exception("Duplicate RelationalListPairs found after InsertRange() completed.");
+        }
+
+        public int LastIndexOfX(T1 x)
+        {
+            int index = -1;
+            for (int i = 0; i < X.Count; i++)
+            {
+                if (X[i].Equals(x)) index = i;
+            }
+            return index;
+        }
+
+        public int LastIndexOfY(T2 y)
+        {
+            int index = -1;
+            for (int i = 0; i < Y.Count; i++)
+            {
+                if (Y[i].Equals(y)) index = i;
+            }
+            return index;
+        }
+
+        public int LastIndexOf(T1 x, T2 y)
+        {
+            int index = -1;
+            for (int i = 0; i < X.Count; i++)
+            {
+                if (X[i].Equals(x) && Y[i].Equals(y)) index = i;
+            }
+            return index;
+        }
+
+        public void CopyTo(object[,] array)
+        {
+            for (int i = 0; i < X.Count; i++)
+            {
+                array[i, 0] = X[i];
+                array[i, 1] = Y[i];
+            }
+        }
+
+        public object[,] ToArray()
+        {
+            object[,] arr = new object[X.Count, 2];
+            CopyTo(arr);
+            return arr;
+        }
+
+        public void RemoveRange(int index, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                RemoveAt(index + i);
+            }
+        }
+
+        public void Reverse()
+        {
+            RelationalList<T1, T2> rl = this;
+            for (int i = X.Count - 2; i > 0; i--)
+            {
+                RelationalListPair<T1, T2> rli = new RelationalListPair<T1, T2>(X[i], Y[i]);
+                rl.RemoveAt(i);
+                rl.Add(rli);
+            }
+            X = rl.X;
+            Y = rl.Y;
+        }
+
+        public void FromDictionary(Dictionary<T1, T2> d)
+        {
+            X = new List<T1>();
+            Y = new List<T2>();
+            foreach (KeyValuePair<T1, T2> kvp in d)
+            {
+                Add(kvp.Key, kvp.Value);
+            }
+        }
+
+        public RelationalList<TX, TY> ConvertTo<TX, TY>() where TX : T1 where TY : T2
+        {
+            RelationalList<TX, TY> rl = new RelationalList<TX, TY>();
+            for (int i = 0; i < X.Count; i++)
+            {
+                rl.Add(new RelationalListPair<TX, TY>((TX)X[i], (TY)Y[i]));
+            }
+            return rl;
+        }
+
+        public RelationalListPair<T1, T2> Last() => new RelationalListPair<T1, T2>(X.Last(), Y.Last());
+        public RelationalListPair<T1, T2> First() => new RelationalListPair<T1, T2>(X.First(), Y.First());
+
+        private bool DuplicatesFound()
+        {
+            for (int i = 1; i < X.Count; i++)
+            {
+                for (int x = 0; x < i; x++)
+                {
+                    if (X[x].Equals(X[i]) && Y[x].Equals(Y[i])) return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerator GetEnumerator() => new RelationalListEnumerator<T1, T2>(this);
+    }
+
+    public class RelationalListEnumerator<T1, T2> : IEnumerator
+    {
+        private RelationalList<T1, T2> RList;
+        private int index;
+
+        public RelationalListEnumerator(RelationalList<T1, T2> rl)
+        {
+            RList = rl;
+            index = -1;
+        }
+
+        public object Current => new RelationalListPair<T1, T2>(RList.X[index], RList.Y[index]);
+
+        public bool MoveNext()
+        {
+            index++;
+            if (index >= RList.Count)
+                return false;
+            else
+                return true;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
---
+++ RelationalList/UnitTests/RelationalList.cs
@@ -0,0 +1,303 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RelationalList;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RelationalListTests
+    {
+        private RelationalList<string, int> Test;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Test = new RelationalList<string, int>()
+            {
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 }
+            };
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestMethod]
+        public void Brackets()
+        {
+            Assert.AreEqual(1, Test["one"]);
+            Assert.AreEqual(2, Test["two"]);
+            Assert.AreEqual(3, Test["three"]);
+            Assert.AreEqual("one", Test[1]);
+            Assert.AreEqual("two", Test[2]);
+            Assert.AreEqual("three", Test[3]);
+        }
+
+        [TestMethod]
+        public void ToRelationalList()
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            string[] strings = new string[] { "one", "two", "three", "four", "five" };
+
+            var test = numbers.ToRelationalList(strings);
+
+            Assert.AreEqual("one", test[1]);
+            Assert.AreEqual("two", test[2]);
+            Assert.AreEqual("three", test[3]);
+            Assert.AreEqual("four", test[4]);
+            Assert.AreEqual("five", test[5]);
+            Assert.AreEqual(1, test.ElementAt(0).X);
+            Assert.AreEqual("five", test.ElementAt(4).Y);
+        }
+
+        [TestMethod]
+        public void ForEach()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (RelationalListPair<string, int> x in Test)
+            {
+                sb.Append(x.X + x.Y.ToString());
+            }
+
+            Assert.AreEqual("one1two2three3four4five5", sb.ToString());
+        }
+
+        [TestMethod]
+        public void UniqueTypes()
+        {
+            try
+            {
+                var test = new RelationalList<string, string>();
+                Assert.Fail();
+            }
+            catch
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void ElementAt()
+        {
+            Assert.AreEqual(1, Test.ElementAt(0).Y);
+            Assert.AreEqual(3, Test.ElementAt(2).Y);
+            Assert.AreEqual(5, Test.ElementAt(4).Y);
+        }
+
+        [TestMethod]
+        public void Add_Individual()
+        {
+            Test.Add("abc", 123);
+
+            Assert.AreEqual(123, Test["abc"]); //test it was added
+            Assert.AreEqual("abc", Test.Last().X); //test it was added *to the end*
+            Assert.AreEqual(6, Test.Count); //test count was increased
+        }
+
+        [TestMethod]
+        public void Add_Pair()
+        {
+            Test.Add(new RelationalListPair<string, int>("abc", 123));
+
+            Assert.AreEqual(123, Test["abc"]); //test it was added
+            Assert.AreEqual("abc", Test.Last().X); //test it was added *to the end*
+            Assert.AreEqual(6, Test.Count); //test count was increased
+        }
+
+        [TestMethod]
+        public void AddRange_Individual()
+        {
+            int c = Test.Count;
+
+            List<string> strings = new List<string> { "six", "seven" };
+            List<int> ints = new List<int> { 6, 7 };
+
+            Test.AddRange(strings, ints);
+
+            Assert.AreEqual(6, Test["six"]); //test it was added
+            Assert.AreEqual(7, Test["seven"]); //test it was added
+            Assert.AreEqual("seven", Test.Last().X); //test it was added *to the end*
+            Assert.AreEqual(7, Test.Count); //test count was increased
+        }
+
+        [TestMethod]
+        public void AddRange_Pair()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Insert_Individual()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Insert_Pair()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void InsertRange_Individual()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void InsertRange_Pair()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Remove_Individual()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Remove_Pair()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void RemoveRange()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void RemoveAt()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Clear()
+        {
+            Test.Clear();
+
+            Assert.AreEqual(0, Test.Count);
+            Assert.AreEqual(0, Test.X.Count);
+            Assert.AreEqual(0, Test.Y.Count);
+        }
+
+        [TestMethod]
+        public void IndexOf_Individual()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void IndexOf_Pair()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Contains_Individual()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Contains_Pair()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void CopyTo()
+        {
+            object[,] arr = new object[,]
+            {
+                { 8, "eight" },
+                { 12893, "twelve thousand eight hundred ninety three" },
+                { 10, "ten" },
+                { 3859892359823395235, "n/a" },
+                { 1, "one more" }
+            };
+            Test.CopyTo(arr);
+
+            Assert.AreEqual("five", arr[4, 0]);
+            Assert.AreEqual("one", arr[0, 0]);
+            Assert.AreEqual(5, arr[4, 1]);
+        }
+
+        [TestMethod]
+        public void ToArray()
+        {
+            var a = Test.ToArray();
+
+            Assert.AreEqual(2, a[1, 1]);
+            Assert.AreEqual("five", a[4, 0]);
+            Assert.AreEqual("three", a[2, 0]);
+        }
+
+        [TestMethod]
+        public void ToList()
+        {
+            var l = Test.ToList();
+
+            Assert.AreEqual("three", l[2].X);
+            Assert.AreEqual(1, l[0].Y);
+            Assert.AreEqual(4, l[3].Y);
+        }
+
+        [TestMethod]
+        public void Reverse()
+        {
+            Test.Reverse();
+
+            Assert.AreEqual(5, Test.First().Y);
+            Assert.AreEqual(1, Test.Last().Y);
+            Assert.AreEqual("three", Test.ElementAt(2).X);
+        }
+
+        [TestMethod]
+        public void Reversed()
+        {
+            var t = Test.Reversed();
+
+            Assert.AreEqual(5, t.First().Y);
+            Assert.AreEqual(1, Test.First().Y);
+            Assert.AreEqual(1, t.Last().Y);
+            Assert.AreEqual(5, Test.Last().Y);
+        }
+
+        [TestMethod]
+        public void FromDictionary()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void ConvertTo()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Duplicates_NotAllowed()
+        {
+            try
+            {
+                Test.Add("one", 1);
+                Assert.Fail();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
